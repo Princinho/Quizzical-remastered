bfb0dd22b367c995a239a026372cce2af67c4eeb
Princinho <princegnakou@gmail.com>
2022-10-03 14:14:14 +0000

    add decode for special characters coming from API

--- src/components/Quizz.js
+++ src/components/Quizz.js
@@ -1,11 +1,12 @@
+import {decode} from 'html-entities';
 export default function Quizz(props) {
     return (<div className="quizz-div">
         {props.data.map(q => {
             return (
                 <div>
-                    <p>{q.question}</p>
+                    <p>{decode(q.question)}</p>
                     <div>
-                        {q.options.map(opt=><button>{opt.answer}</button>)}
+                        {q.options.map(opt=><button>{decode(opt.answer)}</button>)}
                     </div>
                 </div>)
         })}
