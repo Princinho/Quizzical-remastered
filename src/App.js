0f3bc0ed2fe17ac768be6f88d80f7be69824d6f1
Princinho <princegnakou@gmail.com>
2022-10-03 16:16:15 +0000

    enable answers selection

--- src/App.js
+++ src/App.js
@@ -4,26 +4,28 @@ import './App.css';
 import Setup from './components/Setup';
 import Splash from './components/Splash';
 import Quizz from './components/Quizz';
+import { nanoid } from 'nanoid';
 function App() {
   const [showSplash, setShowSplash] = React.useState(true)
   const [showSetupScreen, setShowSetupScreen] = React.useState(false)
   const [gameStarted, setGameStarted] = React.useState(false)
   const [settings, setSettings] = React.useState({ questions: 5, difficulty: "easy", category: "" })
   // const [apiQuestions, setApiQuestions] = React.useState([])
-  const [quizzQuestions,setQuizzQuestions]=React.useState([])
+  const [quizzQuestions, setQuizzQuestions] = React.useState([])
   React.useEffect(() => {
     if (gameStarted) {
       let url = `https://opentdb.com/api.php?amount=${settings.questions}${settings.category ? "&category=" + settings.category : ""}${settings.difficulty ? "&difficulty=" + settings.difficulty : ""}`.trim()
       fetch(url).then(res => res.json()).then(data => {
         // setApiQuestions(data.results)
-         /*Prepare questions for render. Map into a parsedQuestion objects array
-         which can hold answers in an array as objects with a marker to tell which is the right one
-          */
+        /*Prepare questions for render. Map into a parsedQuestion objects array
+        which can hold answers in an array as objects with a marker to tell which is the right one
+         */
         const parsedQuestions = data.results.map(qData => {
-         const parsedQuestion = {
+          const parsedQuestion = {
+            id: nanoid(),
             question: qData.question,
             options: [...qData.incorrect_answers, qData.correct_answer]
-              .map((q, index, arr) => { return { answer: q, correct: index === arr.length - 1 } })
+              .map((q, index, arr) => { return { id:nanoid(),answer: q, correct: index === arr.length - 1,selected:false } })
               .sort(((a, b) => Math.random() - 0.5))
           }
           return parsedQuestion
@@ -47,12 +49,12 @@ function App() {
     setShowSetupScreen(false)
     setGameStarted(true)
   }
-  
+
   return (
     <div className="app">
       {showSplash && <Splash hideSplash={hideSplash} />}
       {showSetupScreen && <Setup settings={settings} startGame={startGame} updateSettings={updateSettings} />}
-      {(gameStarted && quizzQuestions.length>0) && <Quizz data={quizzQuestions}/>}
+      {(gameStarted && quizzQuestions.length > 0) && <Quizz data={quizzQuestions} />}
     </div>
   );
 }
--- src/components/Quizz.js
+++ src/components/Quizz.js
@@ -1,15 +1,31 @@
-import {decode} from 'html-entities';
+import React from 'react';
+import { decode } from 'html-entities';
 export default function Quizz(props) {
-    const [questions,setQuestions]=React.useState(props.data)
+    const [questions, setQuestions] = React.useState(props.data)
+    function handleClick(questionId, answerId) {
+        //map through the questions, update the one that was clicked and set the selected flag to true on it's selected answer.
+        console.log(questionId, answerId)
+        const updatedQuestions = questions
+            .map(q => q.id !== questionId ? q :
+                { ...q, options: q.options.map(o => o.id !== answerId ? { ...o, selected: false } : { ...o, selected: !o.selected }) })
+        console.log(updatedQuestions)
+        setQuestions(updatedQuestions)
+    }
+
+    function checkAnswers(){
+        
+    }
     return (<div className="quizz-div">
-        {questions(q => {
+        {questions.map(q => {
             return (
-                <div className='quizz-question-wrapper'>
+                <div className='quizz-question-wrapper' key={q.id}>
                     <p className='question'>{decode(q.question)}</p>
                     <div className='answers'>
-                        {q.options.map(opt=><button>{decode(opt.answer)}</button>)}
+                        {q.options.map(opt => <button key={opt.id} className={`answer ${opt.selected ? "selected" : ""}`} onClick={() => { handleClick(q.id, opt.id) }}>{decode(opt.answer)}</button>)}
                     </div>
-                </div>)
+                </div>
+            )
         })}
+        <button className='btn-check-answers'>Check answers</button>
     </div>)
 }
